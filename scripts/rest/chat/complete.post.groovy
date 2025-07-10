7b0a6223f22bf54d3f16d8f5a399ef5ee2809e93
admin admin <evaladmin@example.com>
2025-07-09 23:41:44 -0400

    "User admin wrote content /scripts/rest/chat/complete.post.groovy"

--- scripts/rest/chat/complete.post.groovy
+++ scripts/rest/chat/complete.post.groovy
@@ -16,7 +16,7 @@ import org.springframework.ai.chat.client.ChatClient
 // import org.springframework.ai.chat.client.advisor.api.Advisor
 // import org.springframework.ai.chat.client.advisor.api.AdvisorContext
 // import org.springframework.ai.chat.client.advisor.api.AdvisorResponse
-// import org.springframework.ai.chat.client.advisor.api.ChatClientAdvisor
+import org.springframework.ai.chat.client.advisor.api.ChatClientAdvisor
 
 def apiKey = System.getenv("crafter_chatgpt")
 
