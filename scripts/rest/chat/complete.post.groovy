3759d07ede02360265c41f91b34089b7b258f054
admin admin <evaladmin@example.com>
2025-07-09 22:40:05 -0400

    "User admin wrote content /scripts/rest/chat/complete.post.groovy"

--- scripts/rest/chat/complete.post.groovy
+++ scripts/rest/chat/complete.post.groovy
@@ -4,10 +4,11 @@
 @Grab(group='org.springframework.ai', module='spring-ai-core', version='1.0.0-M6', initClass=false)
 @Grab(group='org.springframework.ai', module='spring-ai-openai', version='1.0.0-M6', initClass=false)
 @Grab(group='org.springframework.ai', module='spring-ai-mcp', version='1.0.0-M7', initClass=false)
+@Grab(group='org.springframework.ai', module='spring-ai-starter-model-openai', version='1.0.0-M1', initClass=false)
+
 
 
 // @Grab('org.springframework.boot:spring-boot-starter-web:3.2.5')
-// @Grab('org.springframework.ai:spring-ai-starter-model-openai:1.0.0-M1')
 // @Grab('org.springframework.ai:spring-ai-starter-mcp-client-webflux:1.0.0-M1')
     
 // @Grab(group='org.springframework.ai', module='spring-ai-mcp-client-webflux-spring-boot-starter', version='1.0.0-M6')
